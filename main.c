f9e0bc14af69d49d886a64ca704b04deeff3d71b
Ousmane Traore <hello@ousmane.me>
2020-08-09 01:10:34 -0400

    move white square around the screen with keys

--- main.c
+++ main.c
@@ -1,9 +1,53 @@
 #include "raylib.h"
 
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+Vector2 snakePosition = {0, 0};
+int unitSize = 20;
+int screenWidth, screenHeight;
+float snakeSpeed = 0.2f;
+
+void moveSnake(enum Direction direction, int units, float speed) {
+    Vector2 initialPosition = snakePosition;
+    speed *= units;
+
+    switch (direction) {
+        case UP:
+            snakePosition.y -= speed;
+            break;
+        case DOWN:
+            snakePosition.y += speed;
+            break;
+        case LEFT:
+            snakePosition.x -= speed;
+            break;
+        case RIGHT:
+            snakePosition.x += speed;
+            break;
+    }
+
+    if(snakePosition.x > (screenWidth - units) || snakePosition.x < 0) {
+        snakePosition.x = initialPosition.x;
+    }
+
+    if(snakePosition.y < 0 || snakePosition.y > (screenHeight - units)) {
+        snakePosition.y = initialPosition.y;
+    }
+}
+
 int main()
 {
-    int screenWidth = 800;
-    int screenHeight = 450;
+    screenWidth = 30 * unitSize;
+    screenHeight = 30 * unitSize;
+    Color snakeColor = WHITE;
+    Vector2 snakeUnitSizeV = {unitSize, unitSize};
+    enum Direction startDirection = RIGHT;
+    enum Direction currentDirection = startDirection;
 
     InitWindow(screenWidth, screenHeight, "snake");
 
@@ -11,9 +55,18 @@ int main()
 
     while(!WindowShouldClose())
     {
+        if(currentDirection == LEFT || currentDirection == RIGHT) {
+            if(IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W)) currentDirection = UP;
+            if(IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_S)) currentDirection = DOWN;
+        } else {
+            if(IsKeyPressed(KEY_RIGHT) || IsKeyPressed(KEY_D)) currentDirection = RIGHT;
+            if(IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_A)) currentDirection = LEFT;
+        }
+        moveSnake(currentDirection, unitSize, snakeSpeed);
+
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawText("Hello World", 190, 200, 20, WHITE);
+            DrawRectangleV(snakePosition, snakeUnitSizeV, snakeColor);
         EndDrawing();
     }
 
